9aa25d5eeed93a8e0814ef57744ae30ce11e78d3
Sven Casimir <sven@fewking.de>
2022-01-27 17:58:05 +0100

    fix code

--- src/haspanext/vault.py
+++ src/haspanext/vault.py
@@ -11,14 +11,14 @@ def obtain_config(service, env):
     creds = boto3.Session().get_credentials()
     print('Login via IAM')
     print(vault.auth.aws.iam_login(
-        creds.access_key, creds.secret_key, creds.token, role='service-%-%-iam' % (service, env), header_value='vault.it.paulalex.de'
+        creds.access_key, creds.secret_key, creds.token, role='service-%s-%s-iam' % (service, env), header_value='vault.it.paulalex.de'
     ))
 
     print('Downloading vault secrets')
-    globals.config = vault.secrets.kv.v1.read_secret(
-        path = 'service/%/%' % (service, env)
+    config = vault.secrets.kv.v1.read_secret(
+        path = 'service/%s/%s' % (service, env)
     )['data']
     print('Secrets obtained')
     print('The service name is {name}'.format(
-        name = globals.config['name']
+        name = config['name']
     ))

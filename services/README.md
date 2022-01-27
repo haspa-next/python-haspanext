b5f83ec11179f067f66e1dd88169069ad3a8ec75
Sven Casimir <sven@fewking.de>
2022-01-27 17:44:15 +0100

    initial commit

---
+++ services/README.md
@@ -0,0 +1,4 @@
+# Python Services
+
+This package provides configuration and setup scripts that can be used by services to do common service setup and maintenance tasks.
+Like for example obtaining the service configuration from the vault server.
---
+++ services/src/haspanext/vault.py
@@ -0,0 +1,26 @@
+import sys
+import hvac
+import boto3
+
+service = sys.argv[1]
+env = sys.argv[2]
+
+print('Obtaining vault configuration')
+vault = hvac.Client(
+    url = 'https://vault.it.paulalex.de'
+)
+
+creds = boto3.Session().get_credentials()
+print('Login via IAM')
+print(vault.auth.aws.iam_login(
+    creds.access_key, creds.secret_key, creds.token, role='service-%-%-iam' % (service, env), header_value='vault.it.paulalex.de'
+))
+
+print('Downloading vault secrets')
+globals.config = vault.secrets.kv.v1.read_secret(
+    path = 'service/%/%' % (service, env)
+)['data']
+print('Secrets obtained')
+print('The service name is {name}'.format(
+    name = globals.config['name']
+))

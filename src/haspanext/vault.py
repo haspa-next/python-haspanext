132f6c949fd6f9ae156a9b994316503c9613ff46
Sven Casimir <sven@fewking.de>
2022-01-27 17:54:48 +0100

    fix code

--- src/haspanext/vault.py
+++ src/haspanext/vault.py
@@ -2,25 +2,23 @@ import sys
 import hvac
 import boto3
 
-service = sys.argv[1]
-env = sys.argv[2]
+def obtain_config(service, env):
+    print('Obtaining vault configuration')
+    vault = hvac.Client(
+        url = 'https://vault.it.paulalex.de'
+    )
 
-print('Obtaining vault configuration')
-vault = hvac.Client(
-    url = 'https://vault.it.paulalex.de'
-)
+    creds = boto3.Session().get_credentials()
+    print('Login via IAM')
+    print(vault.auth.aws.iam_login(
+        creds.access_key, creds.secret_key, creds.token, role='service-%-%-iam' % (service, env), header_value='vault.it.paulalex.de'
+    ))
 
-creds = boto3.Session().get_credentials()
-print('Login via IAM')
-print(vault.auth.aws.iam_login(
-    creds.access_key, creds.secret_key, creds.token, role='service-%-%-iam' % (service, env), header_value='vault.it.paulalex.de'
-))
-
-print('Downloading vault secrets')
-globals.config = vault.secrets.kv.v1.read_secret(
-    path = 'service/%/%' % (service, env)
-)['data']
-print('Secrets obtained')
-print('The service name is {name}'.format(
-    name = globals.config['name']
-))
+    print('Downloading vault secrets')
+    globals.config = vault.secrets.kv.v1.read_secret(
+        path = 'service/%/%' % (service, env)
+    )['data']
+    print('Secrets obtained')
+    print('The service name is {name}'.format(
+        name = globals.config['name']
+    ))
